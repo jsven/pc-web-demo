1656ba9eee939359dfc9a188c0f966f1782230d7
liuyong <1059129519@qq.com>
2018-03-09 09:42:18 +0800

    optimize

--- static/Host.js
+++ static/Host.js
@@ -1,7 +1,26 @@
-var _Host = {
+/**
+ * describe 本地开发的服务器配置文件
+ * */
+var _Host = [
+  {
     LOGIN_CONTEXT: '',
     BASE_CONTEXT: '/apis',
     NODE_ENV: '"development"',
-    LOGIN_API:"http://192.168.87.211:10019",   //登录
-    BASE_API: "http://192.168.87.211:10020",   //AJAX接口
-}
+    // 登录
+    LOGIN_API: 'http://192.168.87.211:10019',
+    // AJAX接口
+    BASE_API: 'http://192.168.87.211:10020'
+  },
+  {
+    LOGIN_CONTEXT: '',
+    BASE_CONTEXT: '/apis',
+    NODE_ENV: '"development"',
+    // 登录
+    LOGIN_API: 'http://192.168.87.211:10019',
+    // AJAX接口
+    BASE_API: 'http://192.168.87.211:10020'
+  }
+]
+
+var host = _Host[0]
+console.log(host)
